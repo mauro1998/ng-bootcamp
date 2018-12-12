59f103780da87909397054481672f14dc8c9af15
Mauro Aguilar <maguilar@placester.com>
2018-12-12 14:31:16 -0500

    fix small issue

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  config: {
+    apiKey: 'AIzaSyCZT2RJ8gqy9RHTbY-xtHPmj83jW36M60A',
+    authDomain: 'ng-bootcamp.firebaseapp.com',
+    databaseURL: 'https://ng-bootcamp.firebaseio.com',
+    projectId: 'ng-bootcamp',
+    storageBucket: 'ng-bootcamp.appspot.com',
+    messagingSenderId: '666884588984'
+  }
 };
